854cf8b6986de74eb8aa35ca4de1e7313a30219e
AndriyZaec <andriy_zaec@i.ua>
2015-04-20 00:29:12 +0300

    Look and feel Windows :+1:

--- src/net/ukr/ifkep/oblenergo/Main.java
+++ src/net/ukr/ifkep/oblenergo/Main.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import net.ukr.ifkep.oblenergo.gui.LogoForm;
 import net.ukr.ifkep.oblenergo.gui.MainForm;
@@ -31,6 +33,22 @@ public class Main {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JDialog.setDefaultLookAndFeelDecorated(true);
 		mainForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		try {
+			UIManager.setLookAndFeel(
+			        UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
